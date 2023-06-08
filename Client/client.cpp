2f7cb9f1564b6c3cfffa42723c478dfef0981bbd
NaoDekoNeko <ademarx17@gmail.com>
2023-06-07 21:54:27 -0500

    cliente terminado

--- Client/client.cpp
+++ Client/client.cpp
@@ -6,10 +6,51 @@ Client::Client(QWidget *parent)
     , ui(new Ui::Client)
 {
     ui->setupUi(this);
+    clientSocket = new QTcpSocket(this);
+    in.setDevice(clientSocket);      //Device: podría ser una impresora, una consola
+    connect(ui->btn_close,
+            &QAbstractButton::clicked,
+            this, //por definición es un puntero
+            &QWidget::close
+            );
+    connect(ui->btn_connect,
+            &QAbstractButton::clicked,
+            this, //por definición es un puntero
+            &Client::requestMessage);
+    connect(clientSocket,
+            &QIODevice::readyRead, //esta es la señal que recibe el mensaje del servidory está listo para leer
+            this,
+            &Client::readMessage);
 }
 
 Client::~Client()
 {
     delete ui;
+}
+
+void Client::requestMessage()
+{
+    qInfo() <<"requestMessage() ...";
+    QString ipAddress = ui->le_ip->text();
+    QString port = ui->le_port->text();
+    qInfo() << "ip: " +ipAddress + ", port: " + port;
+    clientSocket->connectToHost(ipAddress,port.toInt());
+}
+
+void Client::readMessage()
+{
+    qInfo() <<"readMessage() ...";
+    in.startTransaction(); //inicia la lectura (transacción)
+    QString message;
+    in >> message; //guarda en memoria (buffer) el mensaje
+    //un buffer es el contenedor donde se almacenan trocitos de un programa muy grande
+    //no se puede almacenar un trozo más grande que lo que el buffer soporta
+    //devuelve true (si se logró almacenar en buffer)
+    //or false (si excedió el tamaño de buffer)
+    if(!in.commitTransaction()){
+        qInfo() << "Error in data stream";
+        return;
+    }
+    ui->lbl_message->setText(message);
 }
 
--- Client/client.h
+++ Client/client.h
@@ -2,6 +2,7 @@
 #define CLIENT_H
 
 #include <QWidget>
+#include <QTcpSocket>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Client; }
@@ -17,5 +18,11 @@ public:
 
 private:
     Ui::Client *ui;
+    QTcpSocket *clientSocket = nullptr; //iniciar el socket de cliente, maneja la comunicación
+    QDataStream in; //donde vamos a manejar el dato que está llegando, se guardan los datos
+    QString receivedMessage; //almacena el mensaje recibido
+private slots:
+    void requestMessage(); //solicita mensaje
+    void readMessage(); //lee el mensaje
 };
 #endif // CLIENT_H
